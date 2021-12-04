d182b2e42d32a650f7506d3d205e96b3a3dbe9b7
timost <dev@timost.fr>
2021-12-04 21:24:24 +0100

    fixup! [WIP] keyboards/thinkpad_kbd: add new keyboard

--- keyboards/thinkpad_kbd/keymaps/default/keymap.c
+++ keyboards/thinkpad_kbd/keymaps/default/keymap.c
@@ -209,14 +209,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,      KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC,   KC_ENT,
         KC_LCTL,       KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_NUHS,
         KC_LSFT,   KC_NUBS, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
-        MO(_OPTS), KC_LCTL,   KC_LGUI, KC_LALT, KC_SPC,                                      OSL(_ALT_GR), KC_PSCR, KC_RCTL, KC_PGUP,KC_UP,  KC_PGDN,
+        _______, KC_LCTL,   KC_LGUI, KC_LALT, KC_SPC,                                      OSL(_ALT_GR), KC_PSCR, KC_RCTL, KC_PGUP,KC_UP,  KC_PGDN,
                                                                                                                              KC_LEFT,KC_DOWN,KC_RGHT,
                                                         _______, _______, _______
     ),
     [_ALT_GR] = LAYOUT(
         _______,    _______, _______, _______, _______,  _______, _______, _______,  _______,  _______, _______, _______, _______,RESET,_______, TO(_OPTS),_______,
         _______,  _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______, OSL(_lCEDIL),  _______,
-        _______,      _______,    _______,    OSL(_EMOJI),    _______,    _______,    _______,    _______,    _______,    _______,    _______,    OSL(_lUML), _______,   _______,
+        _______,      _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    OSL(_lUML), _______,   _______,
         _______,       _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    OSL(_lACUTE), OSL(_lCIRC), OSL(_lAGRAVE),
         _______,   _______, _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______, _______,  _______, _______,
         _______, _______,   _______, _______, _______,                                      _______, _______, _______, _______,_______,  _______,
